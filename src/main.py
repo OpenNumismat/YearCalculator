80034a16957778da7e339cf620a4774d52d59988
JanisV <ignatov.vit@gmail.com>
2023-10-23 15:10:03 +0300

    Integrated i18n

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+from PySide6.QtCore import QLibraryInfo, QLocale, QTranslator
 from PySide6.QtWidgets import QApplication, QDialog
 from YearCalculator import YearCalculatorDialog
 import resources
@@ -6,6 +7,17 @@ import resources
 
 def main():
     app = QApplication(sys.argv)
+
+    locale = QLocale()
+
+    translator = QTranslator(app)
+    if translator.load(locale, 'lang', '_', ':/i18n'):
+        app.installTranslator(translator)
+
+    path = QLibraryInfo.path(QLibraryInfo.TranslationsPath)
+    translator = QTranslator(app)
+    if translator.load(locale, 'qtbase', '_', path):
+        app.installTranslator(translator)
 
     dlg = YearCalculatorDialog('2022', '')
     if dlg.exec() == QDialog.Accepted:
