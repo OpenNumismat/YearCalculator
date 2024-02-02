95aa2ea460bcea7e43ff4ddefc37f1429dad4fe3
JanisV <opennumismat@gmail.com>
2024-02-02 11:52:44 +0300

    Improve misc scripts

--- misc/py2ts.py
+++ misc/py2ts.py
@@ -11,10 +11,11 @@ lupdate_path = os.path.join(pyqt_path, 'lupdate')
 src_files = []
 for dirname, _, filenames in os.walk(base_path):
     for filename in filenames:
-        _, file_extension = os.path.splitext(filename)
-        if file_extension in ('.py', '.ui'):
+        file_title, file_extension = os.path.splitext(filename)
+        if file_extension in ('.py', '.ui') and file_title not in ('resources',):
             src_files.append(os.path.join(dirname, filename))
 
-dst_file = os.path.join(base_path, "i18n/lang.ts")
+i18n_path = os.path.join(os.path.dirname(__file__), "../src/i18n")
+dst_file = os.path.join(i18n_path, "lang.ts")
 os.system(' '.join([lupdate_path, ' '.join(src_files), '-ts', dst_file, '-noobsolete', '-locations', 'none']))
 #os.system(' '.join([lupdate_path, base_path, '-ts', dst_file, '-noobsolete', '-extensions', ','.join(['py', 'ui'])]))
